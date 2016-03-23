f9ad2aa76f7d39034a857a9b79dd80eba92b4a71
Mike Deiters <miked49er@gmail.com>
2016-03-23 13:45:08 -0400

    Built the launcher for the javaFX application

---
+++ blackwing-sy_ths/src/Launcher.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+
+/**
+ * Class: Launcher
+ * @author Mike Deiters
+ * @version 1.0
+ * March 23, 2016
+ * ITEC 3150-01
+ *
+ * Description: Launch the javafx application
+ *
+ * Purpose: Start the application
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        Application.launch(MainGUI.class);
+    }
+}
