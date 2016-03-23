d8a2a45336756942ee68595825d9f67afeef5d1c
Mike Deiters <miked49er@gmail.com>
2016-03-23 13:46:06 -0400

    Built the MainGUI class

--- blackwing-sy_ths/src/MainGUI.java
+++ blackwing-sy_ths/src/MainGUI.java
@@ -1,38 +1,154 @@
+import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.util.ArrayList;
 
-public class MainGUI {
+/**
+ * Class: MainGUI
+ * @author Mike Deiters
+ * @version 1.0
+ * March 23, 2016
+ * ITEC 3860-01
+ *
+ * Description: TODO
+ *
+ * Purpose: TODO
+ */
 
-	private int HEIGHT;
-	private int WIDTH;
-	private int BUTTON_WIDTH;
-	private int BUTTON_HEIGHT;
-	private Scene primaryScene;
-	private Stage mainStage;
-	private BorderPane pane;
-	private GridPane btnGrid;
-	private Data data;
-	private ArrayList btns;
-	private BindPopUp bind;
+public class MainGUI extends Application {
 
-	public void start(Stage args) {
+    private double WIDTH = 1000;
+    private double HEIGHT = 500;
+    private double BUTTON_WIDTH = 100;
+    private double BUTTON_HEIGHT = 100;
+    private Scene primaryScene;
+    private Stage mainStage;
+    private BorderPane pane;
+    private GridPane btnGrid;
+    private Data data;
+    private BindPopUp bind;
+    private ArrayList< Button > btns;
 
-	}
+    public void start( Stage args ) {
 
-	public void setup() {
+        this.pane = new BorderPane();
+        this.primaryScene = new Scene(this.pane, this.WIDTH, this.HEIGHT);
+        this.mainStage = new Stage();
+        this.mainStage.setTitle("Blackwing Sy_ths");
+        this.mainStage.setScene(this.primaryScene);
+        setup();
+        btnActions();
+        this.mainStage.show();
+    }
 
-	}
+    /**
+     * setup
+     * Design and set up the ui
+     */
+    public void setup() {
 
-	public void collectBtns() {
+        this.data = new Data();
+        this.btnGrid = new GridPane();
+        this.btnGrid.setHgap(10);
+        this.btnGrid.setVgap(10);
+        this.bind = new BindPopUp();
 
-	}
+        buildGrid();
+        collectBtns();
 
-	public void btnActions() {
+        for ( int i = 0; i < this.btns.size(); i++ ) {
 
-	}
+            this.btns.get(i).setMaxWidth(this.BUTTON_WIDTH);
+            this.btns.get(i).setMaxHeight(this.BUTTON_HEIGHT);
+        }
+
+        this.pane.setCenter(btnGrid);
+    }
+
+    /**
+     * buildGrid
+     * Build the grid
+     */
+    public void buildGrid() {
+
+        this.btnGrid.add(this.data.Qbtn, 0, 0);
+        this.btnGrid.add(this.data.Wbtn, 0, 1);
+        this.btnGrid.add(this.data.Ebtn, 0, 2);
+        this.btnGrid.add(this.data.Abtn, 1, 0);
+        this.btnGrid.add(this.data.Sbtn, 1, 1);
+        this.btnGrid.add(this.data.Dbtn, 1, 2);
+        this.btnGrid.add(this.data.Zbtn, 2, 0);
+        this.btnGrid.add(this.data.Xbtn, 2, 1);
+        this.btnGrid.add(this.data.Cbtn, 2, 2);
+    }
+
+    /**
+     * collectBtns
+     * Populate the btns arraylist
+     */
+    public void collectBtns() {
+
+        this.btns.add(this.data.Qbtn);
+        this.btns.add(this.data.Wbtn);
+        this.btns.add(this.data.Ebtn);
+        this.btns.add(this.data.Abtn);
+        this.btns.add(this.data.Sbtn);
+        this.btns.add(this.data.Dbtn);
+        this.btns.add(this.data.Zbtn);
+        this.btns.add(this.data.Xbtn);
+        this.btns.add(this.data.Cbtn);
+    }
+
+    /**
+     * btnActions
+     * Add a event listeners to each of the buttons
+     */
+    public void btnActions() {
+
+        for ( int i = 0; i < this.btns.size(); i++ ) {
+
+            Button btn = this.btns.get(i);
+
+            btn.setOnMouseClicked(new EventHandler< MouseEvent >() {
+                @Override
+                public void handle( MouseEvent event ) {
+
+                    bindMusic(btn);
+                }
+            });
+        }
+    }
+
+    /**
+     * bindMusic
+     * @param btn Button
+     * Set up the eventhandler for the button
+     */
+    private void bindMusic(Button btn) {
+
+        String title = "Bind a Sound Bite";
+        String key = btn.getText();
+
+        this.bind.setTitle(title);
+        this.bind.setKey(key);
+        this.bind.setBtn(btn);
+
+        Stage bindStage = new Stage();
+        bindStage.setScene(this.bind);
+        bindStage.show();
+
+        bindStage.setOnCloseRequest(new EventHandler< WindowEvent >() {
+            @Override
+            public void handle( WindowEvent event ) {
+                bindStage.close();
+            }
+        });
+    }
 
 }
