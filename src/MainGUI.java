d8857eb5829be4dc446b4370c11cd364eeec2113
Dave89rr <daleriro@msn.com>
2016-03-30 21:42:24 -0400

    Undo MainGUI Changes
    Accidentally pushed local changes during restructuring. Reverting to correct content.

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,20 +1,13 @@
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.Toggle;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.media.MediaException;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -25,14 +18,14 @@ import java.util.ArrayList;
 
 /**
  * Class: MainGUI
- * @author Mike Deiters, David Rivera
+ * @author Mike Deiters
  * @version 1.0
  * March 23, 2016
  * ITEC 3860-01
  *
- * Description: TODO
+ * Description: Gui Interface for Blackwing Sy_ths
  *
- * Purpose: TODO
+ * Purpose: Create a javafx application
  */
 
 public class MainGUI extends Application {
@@ -48,16 +41,13 @@ public class MainGUI extends Application {
     private Data data;
     private ArrayList<Button> btns;
     private Label error;
-    String toggleOption;
-    private Label toggleTestLabel;
-    final ToggleGroup toggleGroup = new ToggleGroup();
-    HBox toggleHBox = new HBox();
-    VBox vToggleBox = new VBox();
-    Label label = new Label ("Hold and Press Keys for music:");
 
-
-    
-    
+    /**
+     * start
+     * @param primaryStage Stage
+     * @throws Exception
+     * start the application
+     */
     public void start( Stage primaryStage ) throws Exception {
 
         this.pane = new BorderPane();
@@ -68,16 +58,7 @@ public class MainGUI extends Application {
         this.mainStage.setScene(this.primaryScene);
         setup();
         btnActions();
-        keyActions(toggleOption);
-//        if (toggleOption == "on")
-//        {
-//        	keyActionsOnToggle();
-//        }
-//        if (toggleOption == "off")
-//        {
-//        	keyActionsOffToggle();
-//        }
-       
+        keyActions();
         this.mainStage.show();
 
         this.mainStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
@@ -101,11 +82,9 @@ public class MainGUI extends Application {
         this.btnGrid.setVgap(10);
         this.btns = new ArrayList<>();
         this.error = new Label();
-        this.toggleTestLabel = new Label(toggleOption);
 
         buildGrid();
         collectBtns();
-        toggleGroup(primaryScene);
 
         for ( int i = 0; i < this.btns.size(); i++ ) {
 
@@ -115,8 +94,6 @@ public class MainGUI extends Application {
 
         this.pane.setCenter(this.btnGrid);
         this.pane.setBottom(this.error);
-        this.pane.setBottom(this.toggleTestLabel);
-        this.pane.setTop(vToggleBox);
         this.pane.setPadding(new Insets(100, 200, 100, 200));
     }
 
@@ -173,162 +150,66 @@ public class MainGUI extends Application {
             });
         }
     }
-    
-//    public void keyActionsOnToggle(String toggleOption) 
-//    {
-//    	
-//    		this.primaryScene.setOnKeyPressed(new EventHandler<KeyEvent>()
-//    		{
-//    			@Override
-//    			public void handle( KeyEvent event ) {
-//
-//    				Button eventBtn = null;
-//
-//    				switch ( event.getCode() ) {
-//
-//    				case Q: eventBtn = data.Qbtn; eventBtn.playSound(); break;
-//    				case W: eventBtn = data.Wbtn; eventBtn.playSound(); break;
-//    				case E: eventBtn = data.Ebtn; eventBtn.playSound(); break;
-//    				case A: eventBtn = data.Abtn; eventBtn.playSound(); break;
-//    				case S: eventBtn = data.Sbtn; eventBtn.playSound(); break;
-//    				case D: eventBtn = data.Dbtn; eventBtn.playSound(); break;
-//    				case Z: eventBtn = data.Zbtn; eventBtn.playSound(); break;
-//    				case X: eventBtn = data.Xbtn; eventBtn.playSound(); break;
-//    				case C: eventBtn = data.Cbtn; eventBtn.playSound(); break;
-//    				default: break;
-//    				}
-//    			}
-//
-//    		});
-//    		this.primaryScene.setOnKeyReleased(new EventHandler<KeyEvent>()
-//    		{
-//    			@Override
-//    			public void handle( KeyEvent event ) {
-//
-//    				Button eventBtn = null;
-//
-//    				switch ( event.getCode() ) {
-//
-//    				case Q: eventBtn = data.Qbtn; eventBtn.stopSound(); break;
-//    				case W: eventBtn = data.Wbtn; eventBtn.stopSound(); break;
-//    				case E: eventBtn = data.Ebtn; eventBtn.stopSound(); break;
-//    				case A: eventBtn = data.Abtn; eventBtn.stopSound(); break;
-//    				case S: eventBtn = data.Sbtn; eventBtn.stopSound(); break;
-//    				case D: eventBtn = data.Dbtn; eventBtn.stopSound(); break;
-//    				case Z: eventBtn = data.Zbtn; eventBtn.stopSound(); break;
-//    				case X: eventBtn = data.Xbtn; eventBtn.stopSound(); break;
-//    				case C: eventBtn = data.Cbtn; eventBtn.stopSound(); break;
-//    				default: break;
-//    				}
-//    			}
-//    		}); 
-//    }	
-//    
-//    public void keyActionsOffToggle()
-//    {
-//    	
-//    		this.primaryScene.setOnKeyPressed(new EventHandler<KeyEvent>()
-//    		{
-//    			@Override
-//    			public void handle( KeyEvent event ) {
-//
-//    				Button eventBtn = null;
-//
-//    				switch ( event.getCode() ) {
-//
-//    				case Q: eventBtn = data.Qbtn; eventBtn.playSound(); break;
-//    				case W: eventBtn = data.Wbtn; eventBtn.playSound(); break;
-//    				case E: eventBtn = data.Ebtn; eventBtn.playSound(); break;
-//    				case A: eventBtn = data.Abtn; eventBtn.playSound(); break;
-//    				case S: eventBtn = data.Sbtn; eventBtn.playSound(); break;
-//    				case D: eventBtn = data.Dbtn; eventBtn.playSound(); break;
-//    				case Z: eventBtn = data.Zbtn; eventBtn.playSound(); break;
-//    				case X: eventBtn = data.Xbtn; eventBtn.playSound(); break;
-//    				case C: eventBtn = data.Cbtn; eventBtn.playSound(); break;
-//    				default: break;
-//    				}
-//    			}
-//
-//    		}); 
-//    }
-//    
-    
-    public void keyActions(String selectedOption) {
-    	if (selectedOption == "on")
-    	{
-    		this.primaryScene.setOnKeyPressed(new EventHandler<KeyEvent>()
-    		{
-    			@Override
-    			public void handle( KeyEvent event ) {
 
-    				Button eventBtn = null;
+    /**
+     * keyActions
+     * create the key event handlers
+     */
+    public void keyActions() {
 
-    				switch ( event.getCode() ) {
+        this.primaryScene.setOnKeyPressed(new EventHandler<KeyEvent>()
+        {
+			@Override
+            public void handle( KeyEvent event ) {
 
-    				case Q: eventBtn = data.Qbtn; eventBtn.playSound(); break;
-    				case W: eventBtn = data.Wbtn; eventBtn.playSound(); break;
-    				case E: eventBtn = data.Ebtn; eventBtn.playSound(); break;
-    				case A: eventBtn = data.Abtn; eventBtn.playSound(); break;
-    				case S: eventBtn = data.Sbtn; eventBtn.playSound(); break;
-    				case D: eventBtn = data.Dbtn; eventBtn.playSound(); break;
-    				case Z: eventBtn = data.Zbtn; eventBtn.playSound(); break;
-    				case X: eventBtn = data.Xbtn; eventBtn.playSound(); break;
-    				case C: eventBtn = data.Cbtn; eventBtn.playSound(); break;
-    				default: break;
-    				}
-    			}
+                Button eventBtn = null;
 
-    		});
-    		this.primaryScene.setOnKeyReleased(new EventHandler<KeyEvent>()
-    		{
-    			@Override
-    			public void handle( KeyEvent event ) {
+                switch ( event.getCode() ) {
 
-    				Button eventBtn = null;
+                    case Q: eventBtn = data.Qbtn; eventBtn.playSound(); break;
+                    case W: eventBtn = data.Wbtn; eventBtn.playSound(); break;
+                    case E: eventBtn = data.Ebtn; eventBtn.playSound(); break;
+                    case A: eventBtn = data.Abtn; eventBtn.playSound(); break;
+                    case S: eventBtn = data.Sbtn; eventBtn.playSound(); break;
+                    case D: eventBtn = data.Dbtn; eventBtn.playSound(); break;
+                    case Z: eventBtn = data.Zbtn; eventBtn.playSound(); break;
+                    case X: eventBtn = data.Xbtn; eventBtn.playSound(); break;
+                    case C: eventBtn = data.Cbtn; eventBtn.playSound(); break;
+                    default: break;
+                }
+            }
 
-    				switch ( event.getCode() ) {
+        });
+        this.primaryScene.setOnKeyReleased(new EventHandler<KeyEvent>()
+        {
+			@Override
+            public void handle( KeyEvent event ) {
 
-    				case Q: eventBtn = data.Qbtn; eventBtn.stopSound(); break;
-    				case W: eventBtn = data.Wbtn; eventBtn.stopSound(); break;
-    				case E: eventBtn = data.Ebtn; eventBtn.stopSound(); break;
-    				case A: eventBtn = data.Abtn; eventBtn.stopSound(); break;
-    				case S: eventBtn = data.Sbtn; eventBtn.stopSound(); break;
-    				case D: eventBtn = data.Dbtn; eventBtn.stopSound(); break;
-    				case Z: eventBtn = data.Zbtn; eventBtn.stopSound(); break;
-    				case X: eventBtn = data.Xbtn; eventBtn.stopSound(); break;
-    				case C: eventBtn = data.Cbtn; eventBtn.stopSound(); break;
-    				default: break;
-    				}
-    			}
-    		}); 
-    	} else
-    	{
-    		this.primaryScene.setOnKeyPressed(new EventHandler<KeyEvent>()
-    		{
-    			@Override
-    			public void handle( KeyEvent event ) {
+                Button eventBtn = null;
 
-    				Button eventBtn = null;
+                switch ( event.getCode() ) {
 
-    				switch ( event.getCode() ) {
+                    case Q: eventBtn = data.Qbtn; eventBtn.stopSound(); break;
+                    case W: eventBtn = data.Wbtn; eventBtn.stopSound(); break;
+                    case E: eventBtn = data.Ebtn; eventBtn.stopSound(); break;
+                    case A: eventBtn = data.Abtn; eventBtn.stopSound(); break;
+                    case S: eventBtn = data.Sbtn; eventBtn.stopSound(); break;
+                    case D: eventBtn = data.Dbtn; eventBtn.stopSound(); break;
+                    case Z: eventBtn = data.Zbtn; eventBtn.stopSound(); break;
+                    case X: eventBtn = data.Xbtn; eventBtn.stopSound(); break;
+                    case C: eventBtn = data.Cbtn; eventBtn.stopSound(); break;
+                    default: break;
+                }
+            }
 
-    				case Q: eventBtn = data.Qbtn; eventBtn.playSound(); break;
-    				case W: eventBtn = data.Wbtn; eventBtn.playSound(); break;
-    				case E: eventBtn = data.Ebtn; eventBtn.playSound(); break;
-    				case A: eventBtn = data.Abtn; eventBtn.playSound(); break;
-    				case S: eventBtn = data.Sbtn; eventBtn.playSound(); break;
-    				case D: eventBtn = data.Dbtn; eventBtn.playSound(); break;
-    				case Z: eventBtn = data.Zbtn; eventBtn.playSound(); break;
-    				case X: eventBtn = data.Xbtn; eventBtn.playSound(); break;
-    				case C: eventBtn = data.Cbtn; eventBtn.playSound(); break;
-    				default: break;
-    				}
-    			}
-
-    		}); 
-    	}
+        });
     }
 
+    /**
+     * selectFile
+     * @param btn Button
+     * select the music file to assign to button
+     */
     private void selectFile( Button btn ) {
 
         FileChooser fileChooser = new FileChooser();
@@ -350,116 +231,5 @@ public class MainGUI extends Application {
         catch ( MediaException me ) {
             this.error.setText("Unsupported File Type");
         }
-    }
-
-    public void toggleGroup(Scene workScene)
-    {
-    	ToggleButton tb1 = new ToggleButton("On");
-    	tb1.setToggleGroup(toggleGroup);
-    	tb1.setUserData("on");
-    	tb1.setSelected(true);
-    	
-    	ToggleButton tb2 = new ToggleButton("Off");
-    	tb2.setToggleGroup(toggleGroup);
-    	tb2.setUserData("off");
-    	
-    	this.toggleHBox.getChildren().add(tb1);
-    	this.toggleHBox.getChildren().add(tb2);
-    	
-    	vToggleBox.getChildren().add(label);
-    	vToggleBox.getChildren().add(toggleHBox);
-    	
-    	this.vToggleBox.setPadding(new Insets(20, 10, 10, 20));
-    	
-    	 toggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>(){
-         	public void changed(ObservableValue<? extends Toggle> ov,
-         			Toggle toggle, Toggle new_toggle) {
-         		if (new_toggle == null)
-         		{
-         			//Does nothing
-         		} else {
-         			toggleOption = (String) toggleGroup.getSelectedToggle().getUserData();
-         			toggleTestLabel.setText((String) toggleGroup.getSelectedToggle().getUserData());
-         			if (toggleOption == "on")
-         	    	{
-         	    		workScene.setOnKeyPressed(new EventHandler<KeyEvent>()
-         	    		{
-//         	    			@Override
-         	    			public void handle( KeyEvent event ) {
-
-         	    				Button eventBtn = null;
-
-         	    				switch ( event.getCode() ) {
-
-         	    				case Q: eventBtn = data.Qbtn; eventBtn.playSound(); break;
-         	    				case W: eventBtn = data.Wbtn; eventBtn.playSound(); break;
-         	    				case E: eventBtn = data.Ebtn; eventBtn.playSound(); break;
-         	    				case A: eventBtn = data.Abtn; eventBtn.playSound(); break;
-         	    				case S: eventBtn = data.Sbtn; eventBtn.playSound(); break;
-         	    				case D: eventBtn = data.Dbtn; eventBtn.playSound(); break;
-         	    				case Z: eventBtn = data.Zbtn; eventBtn.playSound(); break;
-         	    				case X: eventBtn = data.Xbtn; eventBtn.playSound(); break;
-         	    				case C: eventBtn = data.Cbtn; eventBtn.playSound(); break;
-         	    				default: break;
-         	    				}
-         	    			}
-
-         	    		});
-         	    		workScene.setOnKeyReleased(new EventHandler<KeyEvent>()
-         	    		{
-//         	    			@Override
-         	    			public void handle( KeyEvent event ) {
-
-         	    				Button eventBtn = null;
-
-         	    				switch ( event.getCode() ) {
-
-         	    				case Q: eventBtn = data.Qbtn; eventBtn.stopSound(); break;
-         	    				case W: eventBtn = data.Wbtn; eventBtn.stopSound(); break;
-         	    				case E: eventBtn = data.Ebtn; eventBtn.stopSound(); break;
-         	    				case A: eventBtn = data.Abtn; eventBtn.stopSound(); break;
-         	    				case S: eventBtn = data.Sbtn; eventBtn.stopSound(); break;
-         	    				case D: eventBtn = data.Dbtn; eventBtn.stopSound(); break;
-         	    				case Z: eventBtn = data.Zbtn; eventBtn.stopSound(); break;
-         	    				case X: eventBtn = data.Xbtn; eventBtn.stopSound(); break;
-         	    				case C: eventBtn = data.Cbtn; eventBtn.stopSound(); break;
-         	    				default: break;
-         	    				}
-         	    			}
-         	    		}); 
-         	    	} else
-         	    	{
-         	    		workScene.setOnKeyPressed(new EventHandler<KeyEvent>()
-         	    		{
-//         	    			@Override
-         	    			public void handle( KeyEvent event2 ) {
-
-         	    				Button eventBtn2 = null;
-
-         	    				switch ( event2.getCode() ) {
-
-         	    				case Q: eventBtn2 = data.Qbtn; eventBtn2.playSound(); break;
-         	    				case W: eventBtn2 = data.Wbtn; eventBtn2.playSound(); break;
-         	    				case E: eventBtn2 = data.Ebtn; eventBtn2.playSound(); break;
-         	    				case A: eventBtn2 = data.Abtn; eventBtn2.playSound(); break;
-         	    				case S: eventBtn2 = data.Sbtn; eventBtn2.playSound(); break;
-         	    				case D: eventBtn2 = data.Dbtn; eventBtn2.playSound(); break;
-         	    				case Z: eventBtn2 = data.Zbtn; eventBtn2.playSound(); break;
-         	    				case X: eventBtn2 = data.Xbtn; eventBtn2.playSound(); break;
-         	    				case C: eventBtn2 = data.Cbtn; eventBtn2.playSound(); break;
-         	    				default: break;
-         	    				}
-         	    			}
-
-         	    		}); 
-         	    	}
-         		}
-         	}
-         });
-    }
-    
-    public Scene getScene()
-    {
-    	return primaryScene;
     }
 }
