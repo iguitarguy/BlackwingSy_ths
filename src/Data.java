85644d5e3ec973235672ce242781d8a6c77e6696
Mike Deiters <miked49er@gmail.com>
2016-04-01 00:22:53 -0400

    Created the reset of the buttons

--- src/Data.java
+++ src/Data.java
@@ -1,25 +1,79 @@
 public class Data {
 
+	public Button N1btn;
+	public Button N2btn;
+	public Button N3btn;
+	public Button N4btn;
+	public Button N5btn;
+	public Button N6btn;
+	public Button N7btn;
+	public Button N8btn;
+	public Button N9btn;
+	public Button N0btn;
 	public Button Qbtn;
 	public Button Wbtn;
 	public Button Ebtn;
+	public Button Rbtn;
+	public Button Tbtn;
+	public Button Ybtn;
+	public Button Ubtn;
+	public Button Ibtn;
+	public Button Obtn;
+	public Button Pbtn;
 	public Button Abtn;
 	public Button Sbtn;
 	public Button Dbtn;
+	public Button Fbtn;
+	public Button Gbtn;
+	public Button Hbtn;
+	public Button Jbtn;
+	public Button Kbtn;
+	public Button Lbtn;
 	public Button Zbtn;
 	public Button Xbtn;
 	public Button Cbtn;
+	public Button Vbtn;
+	public Button Bbtn;
+	public Button Nbtn;
+	public Button Mbtn;
 
 	public Data() {
+		N1btn = new Button("1");
+		N2btn = new Button("2");
+		N3btn = new Button("3");
+		N4btn = new Button("4");
+		N5btn = new Button("5");
+		N6btn = new Button("6");
+		N7btn = new Button("7");
+		N8btn = new Button("8");
+		N9btn = new Button("9");
+		N0btn = new Button("0");
 		Qbtn = new Button("Q");
 		Wbtn = new Button("W");
 		Ebtn = new Button("E");
+		Rbtn = new Button("R");
+		Tbtn = new Button("T");
+		Ybtn = new Button("Y");
+		Ubtn = new Button("U");
+		Ibtn = new Button("I");
+		Obtn = new Button("O");
+		Pbtn = new Button("P");
 		Abtn = new Button("A");
 		Sbtn = new Button("S");
 		Dbtn = new Button("D");
+		Fbtn = new Button("F");
+		Gbtn = new Button("G");
+		Hbtn = new Button("H");
+		Jbtn = new Button("J");
+		Kbtn = new Button("K");
+		Lbtn = new Button("L");
 		Zbtn = new Button("Z");
 		Xbtn = new Button("X");
 		Cbtn = new Button("C");
+		Vbtn = new Button("V");
+		Bbtn = new Button("B");
+		Nbtn = new Button("N");
+		Mbtn = new Button("M");
 	}
 
 }
