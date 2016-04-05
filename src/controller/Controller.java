b040679eb1109b4e3dbdfc86a0ddb316432a1afe
Dave89rr <daleriro@msn.com>
2016-04-05 16:17:47 -0400

    Clean Up
    Formatted the controller class to take up less lines
    Removed unused ThreadsChangingGui experimental class
    Removed Unused MainGUI
    Removed Unused Launcher

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -166,146 +166,53 @@ public class Controller implements Initializable {
     protected void playMusic( KeyEvent event ) {
 
         switch ( event.getCode() ) {
-            case DIGIT1:
-                N1btn.playSound();
-                break;
-            case DIGIT2:
-                N2btn.playSound();
-                break;
-            case DIGIT3:
-                N3btn.playSound();
-                break;
-            case DIGIT4:
-                N4btn.playSound();
-                break;
-            case DIGIT5:
-                N5btn.playSound();
-                break;
-            case DIGIT6:
-                N6btn.playSound();
-                break;
-            case DIGIT7:
-                N7btn.playSound();
-                break;
-            case DIGIT8:
-                N8btn.playSound();
-                break;
-            case DIGIT9:
-                N9btn.playSound();
-                break;
-            case DIGIT0:
-                N0btn.playSound();
-                break;
-            case NUMPAD1:
-                N1btn.playSound();
-                break;
-            case NUMPAD2:
-                N2btn.playSound();
-                break;
-            case NUMPAD3:
-                N3btn.playSound();
-                break;
-            case NUMPAD4:
-                N4btn.playSound();
-                break;
-            case NUMPAD5:
-                N5btn.playSound();
-                break;
-            case NUMPAD6:
-                N6btn.playSound();
-                break;
-            case NUMPAD7:
-                N7btn.playSound();
-                break;
-            case NUMPAD8:
-                N8btn.playSound();
-                break;
-            case NUMPAD9:
-                N9btn.playSound();
-                break;
-            case NUMPAD0:
-                N0btn.playSound();
-                break;
-            case Q:
-                Qbtn.playSound();
-                break;
-            case W:
-                Wbtn.playSound();
-                break;
-            case E:
-                Ebtn.playSound();
-                break;
-            case R:
-                Rbtn.playSound();
-                break;
-            case T:
-                Tbtn.playSound();
-                break;
-            case Y:
-                Ybtn.playSound();
-                break;
-            case U:
-                Ubtn.playSound();
-                break;
-            case I:
-                Ibtn.playSound();
-                break;
-            case O:
-                Obtn.playSound();
-                break;
-            case P:
-                Pbtn.playSound();
-                break;
-            case A:
-                Abtn.playSound();
-                break;
-            case S:
-                Sbtn.playSound();
-                break;
-            case D:
-                Dbtn.playSound();
-                break;
-            case F:
-                Fbtn.playSound();
-                break;
-            case G:
-                Gbtn.playSound();
-                break;
-            case H:
-                Hbtn.playSound();
-                break;
-            case J:
-                Jbtn.playSound();
-                break;
-            case K:
-                Kbtn.playSound();
-                break;
-            case L:
-                Lbtn.playSound();
-                break;
-            case Z:
-                Zbtn.playSound();
-                break;
-            case X:
-                Xbtn.playSound();
-                break;
-            case C:
-                Cbtn.playSound();
-                break;
-            case V:
-                Vbtn.playSound();
-                break;
-            case B:
-                Bbtn.playSound();
-                break;
-            case N:
-                Nbtn.playSound();
-                break;
-            case M:
-                Mbtn.playSound();
-                break;
-            default:
-                break;
+            case DIGIT1: N1btn.playSound(); break;
+            case DIGIT2: N2btn.playSound(); break;
+            case DIGIT3: N3btn.playSound(); break;
+            case DIGIT4: N4btn.playSound(); break;
+            case DIGIT5: N5btn.playSound(); break;
+            case DIGIT6: N6btn.playSound(); break;
+            case DIGIT7: N7btn.playSound(); break;
+            case DIGIT8: N8btn.playSound(); break;
+            case DIGIT9: N9btn.playSound(); break;
+            case DIGIT0: N0btn.playSound(); break;
+            case NUMPAD1: N1btn.playSound(); break;
+            case NUMPAD2: N2btn.playSound(); break;
+            case NUMPAD3: N3btn.playSound(); break;
+            case NUMPAD4: N4btn.playSound(); break;
+            case NUMPAD5: N5btn.playSound(); break;
+            case NUMPAD6: N6btn.playSound(); break;
+            case NUMPAD7: N7btn.playSound(); break;
+            case NUMPAD8: N8btn.playSound(); break;
+            case NUMPAD9: N9btn.playSound(); break;
+            case NUMPAD0: N0btn.playSound(); break;
+            case A: Abtn.playSound(); break;
+            case B: Bbtn.playSound(); break;
+            case C: Cbtn.playSound(); break;
+            case D: Dbtn.playSound(); break;
+            case E: Ebtn.playSound(); break;
+            case F: Fbtn.playSound(); break;
+            case G: Gbtn.playSound(); break;
+            case H: Hbtn.playSound(); break;
+            case I: Ibtn.playSound(); break;
+            case J: Jbtn.playSound(); break;
+            case K: Kbtn.playSound(); break;
+            case L: Lbtn.playSound(); break;
+            case M: Mbtn.playSound(); break;
+            case N: Nbtn.playSound(); break;
+            case O: Obtn.playSound(); break;
+            case P: Pbtn.playSound(); break;
+            case Q: Qbtn.playSound(); break;
+            case R: Rbtn.playSound(); break;
+            case S: Sbtn.playSound(); break;
+            case T: Tbtn.playSound(); break;
+            case U: Ubtn.playSound(); break;
+            case V: Vbtn.playSound(); break;
+            case W: Wbtn.playSound(); break;
+            case X: Xbtn.playSound(); break;
+            case Y: Ybtn.playSound(); break;
+            case Z: Zbtn.playSound(); break;
+            default: break;
         }
     }
 
@@ -313,146 +220,53 @@ public class Controller implements Initializable {
     protected void stopMusic( KeyEvent event ) {
 
         switch ( event.getCode() ) {
-            case DIGIT1:
-                N1btn.stopSound();
-                break;
-            case DIGIT2:
-                N2btn.stopSound();
-                break;
-            case DIGIT3:
-                N3btn.stopSound();
-                break;
-            case DIGIT4:
-                N4btn.stopSound();
-                break;
-            case DIGIT5:
-                N5btn.stopSound();
-                break;
-            case DIGIT6:
-                N6btn.stopSound();
-                break;
-            case DIGIT7:
-                N7btn.stopSound();
-                break;
-            case DIGIT8:
-                N8btn.stopSound();
-                break;
-            case DIGIT9:
-                N9btn.stopSound();
-                break;
-            case DIGIT0:
-                N0btn.stopSound();
-                break;
-            case NUMPAD1:
-                N1btn.stopSound();
-                break;
-            case NUMPAD2:
-                N2btn.stopSound();
-                break;
-            case NUMPAD3:
-                N3btn.stopSound();
-                break;
-            case NUMPAD4:
-                N4btn.stopSound();
-                break;
-            case NUMPAD5:
-                N5btn.stopSound();
-                break;
-            case NUMPAD6:
-                N6btn.stopSound();
-                break;
-            case NUMPAD7:
-                N7btn.stopSound();
-                break;
-            case NUMPAD8:
-                N8btn.stopSound();
-                break;
-            case NUMPAD9:
-                N9btn.stopSound();
-                break;
-            case NUMPAD0:
-                N0btn.stopSound();
-                break;
-            case Q:
-                Qbtn.stopSound();
-                break;
-            case W:
-                Wbtn.stopSound();
-                break;
-            case E:
-                Ebtn.stopSound();
-                break;
-            case R:
-                Rbtn.stopSound();
-                break;
-            case T:
-                Tbtn.stopSound();
-                break;
-            case Y:
-                Ybtn.stopSound();
-                break;
-            case U:
-                Ubtn.stopSound();
-                break;
-            case I:
-                Ibtn.stopSound();
-                break;
-            case O:
-                Obtn.stopSound();
-                break;
-            case P:
-                Pbtn.stopSound();
-                break;
-            case A:
-                Abtn.stopSound();
-                break;
-            case S:
-                Sbtn.stopSound();
-                break;
-            case D:
-                Dbtn.stopSound();
-                break;
-            case F:
-                Fbtn.stopSound();
-                break;
-            case G:
-                Gbtn.stopSound();
-                break;
-            case H:
-                Hbtn.stopSound();
-                break;
-            case J:
-                Jbtn.stopSound();
-                break;
-            case K:
-                Kbtn.stopSound();
-                break;
-            case L:
-                Lbtn.stopSound();
-                break;
-            case Z:
-                Zbtn.stopSound();
-                break;
-            case X:
-                Xbtn.stopSound();
-                break;
-            case C:
-                Cbtn.stopSound();
-                break;
-            case V:
-                Vbtn.stopSound();
-                break;
-            case B:
-                Bbtn.stopSound();
-                break;
-            case N:
-                Nbtn.stopSound();
-                break;
-            case M:
-                Mbtn.stopSound();
-                break;
-            default:
-                break;
+            case DIGIT1: N1btn.stopSound(); break;
+            case DIGIT2: N2btn.stopSound(); break;
+            case DIGIT3: N3btn.stopSound(); break;
+            case DIGIT4: N4btn.stopSound(); break;
+            case DIGIT5: N5btn.stopSound(); break;
+            case DIGIT6: N6btn.stopSound(); break;
+            case DIGIT7: N7btn.stopSound(); break;
+            case DIGIT8: N8btn.stopSound(); break;
+            case DIGIT9: N9btn.stopSound(); break;
+            case DIGIT0: N0btn.stopSound(); break;
+            case NUMPAD1: N1btn.stopSound(); break;
+            case NUMPAD2: N2btn.stopSound(); break;
+            case NUMPAD3: N3btn.stopSound(); break;
+            case NUMPAD4: N4btn.stopSound(); break;
+            case NUMPAD5: N5btn.stopSound(); break;
+            case NUMPAD6: N6btn.stopSound(); break;
+            case NUMPAD7: N7btn.stopSound(); break;
+            case NUMPAD8: N8btn.stopSound(); break;
+            case NUMPAD9: N9btn.stopSound(); break;
+            case NUMPAD0: N0btn.stopSound(); break;
+            case Q: Qbtn.stopSound(); break;
+            case W: Wbtn.stopSound(); break;
+            case E: Ebtn.stopSound(); break;
+            case R: Rbtn.stopSound(); break;
+            case T: Tbtn.stopSound(); break;
+            case Y: Ybtn.stopSound(); break;
+            case U: Ubtn.stopSound(); break;
+            case I: Ibtn.stopSound(); break;
+            case O: Obtn.stopSound(); break;
+            case P: Pbtn.stopSound(); break;
+            case A: Abtn.stopSound(); break;
+            case S: Sbtn.stopSound(); break;
+            case D: Dbtn.stopSound(); break;
+            case F: Fbtn.stopSound(); break;
+            case G: Gbtn.stopSound(); break;
+            case H: Hbtn.stopSound(); break;
+            case J: Jbtn.stopSound(); break;
+            case K: Kbtn.stopSound(); break;
+            case L: Lbtn.stopSound(); break;
+            case Z: Zbtn.stopSound(); break;
+            case X: Xbtn.stopSound(); break;
+            case C: Cbtn.stopSound(); break;
+            case V: Vbtn.stopSound(); break;
+            case B: Bbtn.stopSound(); break;
+            case N: Nbtn.stopSound(); break;
+            case M: Mbtn.stopSound(); break;
+            default: break;
         }
     }
 
