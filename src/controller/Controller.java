8e77e1c1f5b3a1b666dcf53d5488919e80d07c2c
iguitarguy <atsou@ggc.edu>
2016-04-06 17:45:38 -0400

    Made right click event pull up bin\Composite

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -111,6 +111,30 @@ public class Controller implements Initializable {
                 this.error.setText("Unsupported File Type");
             }
         }
+        else if (event.getButton().equals(MouseButton.SECONDARY)) {
+        	System.out.println("Right mouse clicked.");
+        	 Button btn = (Button) event.getSource();
+
+             FileChooser fileChooser = new FileChooser();
+             fileChooser.setInitialDirectory(new File(System.getProperty("user.home") + System.getProperty("file.separator") + "workspace/Blackwing Sy_ths/bin/Composite"));
+             fileChooser.setTitle("Select Music Bite");
+
+             fileChooser.getExtensionFilters().addAll(
+                     new FileChooser.ExtensionFilter("All Music", "*.*"),
+                     new FileChooser.ExtensionFilter("MP3", "*.mp3"));
+           
+             try {
+                 File file = fileChooser.showOpenDialog(null);
+                 btn.setSound(file);
+                 this.error.setText("");
+             }
+             catch ( NullPointerException npe ) {
+                 this.error.setText("No File Selected");
+             }
+             catch ( MediaException me ) {
+                 this.error.setText("Unsupported File Type");
+             }
+        }
     }
 
     /**
