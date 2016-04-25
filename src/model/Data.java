ccce1492b75816230e94f83a38ba706cce6e1815
Mike Deiters <miked49er@gmail.com>
2016-04-24 20:14:47 -0400

    Hard coded the sound files

--- src/model/Data.java
+++ src/model/Data.java
@@ -1,81 +1,269 @@
 package model;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Data {
 
-	public Button N1btn;
-	public Button N2btn;
-	public Button N3btn;
-	public Button N4btn;
-	public Button N5btn;
-	public Button N6btn;
-	public Button N7btn;
-	public Button N8btn;
-	public Button N9btn;
-	public Button N0btn;
-	public Button Qbtn;
-	public Button Wbtn;
-	public Button Ebtn;
-	public Button Rbtn;
-	public Button Tbtn;
-	public Button Ybtn;
-	public Button Ubtn;
-	public Button Ibtn;
-	public Button Obtn;
-	public Button Pbtn;
-	public Button Abtn;
-	public Button Sbtn;
-	public Button Dbtn;
-	public Button Fbtn;
-	public Button Gbtn;
-	public Button Hbtn;
-	public Button Jbtn;
-	public Button Kbtn;
-	public Button Lbtn;
-	public Button Zbtn;
-	public Button Xbtn;
-	public Button Cbtn;
-	public Button Vbtn;
-	public Button Bbtn;
-	public Button Nbtn;
-	public Button Mbtn;
+    public ArrayList< String > hatP;
+    public ArrayList< String > kickP;
+    public ArrayList< String > snareP;
+    public ArrayList< String > bLoopP;
+    public ArrayList< String > dLoopP;
+    public ArrayList< String > pLoopP;
+    public ArrayList< String > sfxP;
+    public ArrayList< String > vocalP;
+    public ArrayList< String > allP;
 
-	public Data() {
-		N1btn = new Button("1");
-		N2btn = new Button("2");
-		N3btn = new Button("3");
-		N4btn = new Button("4");
-		N5btn = new Button("5");
-		N6btn = new Button("6");
-		N7btn = new Button("7");
-		N8btn = new Button("8");
-		N9btn = new Button("9");
-		N0btn = new Button("0");
-		Qbtn = new Button("Q");
-		Wbtn = new Button("W");
-		Ebtn = new Button("E");
-		Rbtn = new Button("R");
-		Tbtn = new Button("T");
-		Ybtn = new Button("Y");
-		Ubtn = new Button("U");
-		Ibtn = new Button("I");
-		Obtn = new Button("O");
-		Pbtn = new Button("P");
-		Abtn = new Button("A");
-		Sbtn = new Button("S");
-		Dbtn = new Button("D");
-		Fbtn = new Button("F");
-		Gbtn = new Button("G");
-		Hbtn = new Button("H");
-		Jbtn = new Button("J");
-		Kbtn = new Button("K");
-		Lbtn = new Button("L");
-		Zbtn = new Button("Z");
-		Xbtn = new Button("X");
-		Cbtn = new Button("C");
-		Vbtn = new Button("V");
-		Bbtn = new Button("B");
-		Nbtn = new Button("N");
-		Mbtn = new Button("M");
-	}
+    public Data() throws Exception {
+
+        hatP = new ArrayList<>();
+        kickP = new ArrayList<>();
+        snareP = new ArrayList<>();
+        bLoopP = new ArrayList<>();
+        dLoopP = new ArrayList<>();
+        pLoopP = new ArrayList<>();
+        sfxP = new ArrayList<>();
+        vocalP = new ArrayList<>();
+        allP = new ArrayList<>();
+
+        String OS = System.getProperty("os.name");
+        if ( OS.startsWith("Windows") ) {
+
+            windowsFiles();
+        }
+        else {
+
+            unixFiles();
+        }
+    }
+
+    private void unixFiles() throws Exception {
+
+        String[] hat = new String[] {
+                "/bin/Composite/Drums/Hat/Hat (1).mp3",
+                "/bin/Composite/Drums/Hat/Hat (2).mp3",
+                "/bin/Composite/Drums/Hat/Hat (3).mp3",
+                "/bin/Composite/Drums/Hat/Hat (4).mp3",
+                "/bin/Composite/Drums/Hat/Hat (5).mp3",
+                "/bin/Composite/Drums/Hat/Hat (6).mp3",
+                "/bin/Composite/Drums/Hat/Hat (7).mp3",
+                "/bin/Composite/Drums/Hat/Hat (8).mp3",
+                "/bin/Composite/Drums/Hat/Hat (9).mp3",
+                "/bin/Composite/Drums/Hat/Hat (10).mp3"
+        };
+        String[] kick = new String[] {
+                "/bin/Composite/Drums/Kick/Kick (1).mp3",
+                "/bin/Composite/Drums/Kick/Kick (2).mp3",
+                "/bin/Composite/Drums/Kick/Kick (3).mp3",
+                "/bin/Composite/Drums/Kick/Kick (4).mp3",
+                "/bin/Composite/Drums/Kick/Kick (5).mp3",
+                "/bin/Composite/Drums/Kick/Kick (7).mp3",
+                "/bin/Composite/Drums/Kick/Kick (8).mp3",
+                "/bin/Composite/Drums/Kick/Kick (9).mp3",
+                "/bin/Composite/Drums/Kick/Kick (10).mp3",
+                "/bin/Composite/Drums/Kick/Kick (6).mp3",
+                "/bin/Composite/Drums/Kick/Kick (11).mp3",
+                "/bin/Composite/Drums/Kick/Kick (12).mp3",
+                "/bin/Composite/Drums/Kick/Kick (13).mp3",
+                "/bin/Composite/Drums/Kick/Kick (14).mp3"
+        };
+        String[] snare = new String[] {
+                "/bin/Composite/Drums/Snare/AcousticSnare (1).mp3",
+                "/bin/Composite/Drums/Snare/AcousticSnare (2).mp3",
+                "/bin/Composite/Drums/Snare/AcousticSnare (3).mp3",
+                "/bin/Composite/Drums/Snare/AcousticSnare (4).mp3",
+                "/bin/Composite/Drums/Snare/AcousticSnare (5).mp3",
+                "/bin/Composite/Drums/Snare/AcousticSnare (6).mp3",
+                "/bin/Composite/Drums/Snare/AcousticSnare (7).mp3",
+                "/bin/Composite/Drums/Snare/AcousticSnare (8).mp3",
+                "/bin/Composite/Drums/Snare/AcousticSnare (9).mp3",
+                "/bin/Composite/Drums/Snare/Roll.mp3",
+                "/bin/Composite/Drums/Snare/Snare (1).mp3",
+                "/bin/Composite/Drums/Snare/Snare (2).mp3",
+                "/bin/Composite/Drums/Snare/SynthSnare (1).mp3",
+                "/bin/Composite/Drums/Snare/SynthSnare (2).mp3",
+                "/bin/Composite/Drums/Snare/SynthSnare (3).mp3",
+                "/bin/Composite/Drums/Snare/SynthSnare (4).mp3",
+                "/bin/Composite/Drums/Snare/SynthSnare (5).mp3",
+                "/bin/Composite/Drums/Snare/SynthSnare (6).mp3",
+                "/bin/Composite/Drums/Snare/SynthSnare (7).mp3"
+        };
+        String[] bLoop = new String[] {
+                "/bin/Composite/Loops/Bass/BassLoop (1).mp3",
+                "/bin/Composite/Loops/Bass/BassLoop (2).mp3",
+                "/bin/Composite/Loops/Bass/BassLoop (3).mp3"
+        };
+        String[] dLoop = new String[] {
+                "/bin/Composite/Loops/Drum/DrumLoop (1).mp3",
+                "/bin/Composite/Loops/Drum/DrumLoop (2).mp3",
+                "/bin/Composite/Loops/Drum/DrumLoop (3).mp3",
+                "/bin/Composite/Loops/Drum/DrumLoop (4).mp3",
+                "/bin/Composite/Loops/Drum/DrumPad (1).mp3",
+                "/bin/Composite/Loops/Drum/DrumPad (2).mp3",
+                "/bin/Composite/Loops/Drum/DrumPad (3).mp3",
+                "/bin/Composite/Loops/Drum/DrumPad (4).mp3"
+        };
+        String[] pLoop = new String[] {
+                "/bin/Composite/Loops/Piano/Piano (1).mp3",
+                "/bin/Composite/Loops/Piano/Piano (5).mp3",
+                "/bin/Composite/Loops/Piano/Piano (6).mp3",
+                "/bin/Composite/Loops/Piano/Piano (7).mp3"
+        };
+        String[] sfx = new String[] {
+                "/bin/Composite/SFX/Beat.mp3",
+                "/bin/Composite/SFX/Clap (1).mp3",
+                "/bin/Composite/SFX/Clap (2).mp3",
+                "/bin/Composite/SFX/Increase (1).mp3",
+                "/bin/Composite/SFX/Increase (2).mp3",
+                "/bin/Composite/SFX/Long.mp3",
+                "/bin/Composite/SFX/Noise.mp3",
+                "/bin/Composite/SFX/Organ.mp3",
+                "/bin/Composite/SFX/Short.mp3",
+                "/bin/Composite/SFX/Staccato.mp3"
+        };
+        String[] vocal = new String[] {
+                "/bin/Composite/Vocals/PayAttention.mp3",
+                "/bin/Composite/Vocals/Rrrah.mp3"
+        };
+
+        createLists(hat, kick, snare, bLoop, dLoop, pLoop, sfx, vocal);
+    }
+
+    private void windowsFiles() throws Exception {
+
+        String[] hat = new String[] {
+                "\\bin\\Composite\\Drums\\Hat\\Hat (1).mp3",
+                "\\bin\\Composite\\Drums\\Hat\\Hat (2).mp3",
+                "\\bin\\Composite\\Drums\\Hat\\Hat (3).mp3",
+                "\\bin\\Composite\\Drums\\Hat\\Hat (4).mp3",
+                "\\bin\\Composite\\Drums\\Hat\\Hat (5).mp3",
+                "\\bin\\Composite\\Drums\\Hat\\Hat (6).mp3",
+                "\\bin\\Composite\\Drums\\Hat\\Hat (7).mp3",
+                "\\bin\\Composite\\Drums\\Hat\\Hat (8).mp3",
+                "\\bin\\Composite\\Drums\\Hat\\Hat (9).mp3",
+                "\\bin\\Composite\\Drums\\Hat\\Hat (10).mp3"
+        };
+        String[] kick = new String[] {
+                "\\bin\\Composite\\Drums\\Kick\\Kick (1).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (2).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (3).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (4).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (5).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (6).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (7).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (8).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (9).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (10).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (11).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (12).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (13).mp3",
+                "\\bin\\Composite\\Drums\\Kick\\Kick (14).mp3"
+        };
+        String[] snare = new String[] {
+                "\\bin\\Composite\\Drums\\Snare\\AcousticSnare (1).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\AcousticSnare (2).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\AcousticSnare (3).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\AcousticSnare (4).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\AcousticSnare (5).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\AcousticSnare (6).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\AcousticSnare (7).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\AcousticSnare (8).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\AcousticSnare (9).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\Roll.mp3",
+                "\\bin\\Composite\\Drums\\Snare\\Snare (1).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\Snare (2).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\SynthSnare (1).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\SynthSnare (2).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\SynthSnare (3).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\SynthSnare (4).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\SynthSnare (5).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\SynthSnare (6).mp3",
+                "\\bin\\Composite\\Drums\\Snare\\SynthSnare (7).mp3"
+        };
+        String[] bLoop = new String[] {
+                "\\bin\\Composite\\Loops\\Bass\\BassLoop (1).mp3",
+                "\\bin\\Composite\\Loops\\Bass\\BassLoop (2).mp3",
+                "\\bin\\Composite\\Loops\\Bass\\BassLoop (3).mp3"
+        };
+        String[] dLoop = new String[] {
+                "\\bin\\Composite\\Loops\\Drum\\DrumLoop (1).mp3",
+                "\\bin\\Composite\\Loops\\Drum\\DrumLoop (2).mp3",
+                "\\bin\\Composite\\Loops\\Drum\\DrumLoop (3).mp3",
+                "\\bin\\Composite\\Loops\\Drum\\DrumLoop (4).mp3",
+                "\\bin\\Composite\\Loops\\Drum\\DrumPad (1).mp3",
+                "\\bin\\Composite\\Loops\\Drum\\DrumPad (2).mp3",
+                "\\bin\\Composite\\Loops\\Drum\\DrumPad (3).mp3",
+                "\\bin\\Composite\\Loops\\Drum\\DrumPad (4).mp3"
+        };
+        String[] pLoop = new String[] {
+                "\\bin\\Composite\\Loops\\Piano\\Piano (1).mp3",
+                "\\bin\\Composite\\Loops\\Piano\\Piano (5).mp3",
+                "\\bin\\Composite\\Loops\\Piano\\Piano (6).mp3",
+                "\\bin\\Composite\\Loops\\Piano\\Piano (7).mp3",
+        };
+        String[] sfx = new String[] {
+                "\\bin\\Composite\\SFX\\Beat.mp3",
+                "\\bin\\Composite\\SFX\\Clap (1).mp3",
+                "\\bin\\Composite\\SFX\\Clap (2).mp3",
+                "\\bin\\Composite\\SFX\\Increase (1).mp3",
+                "\\bin\\Composite\\SFX\\Increase (2).mp3",
+                "\\bin\\Composite\\SFX\\Long.mp3",
+                "\\bin\\Composite\\SFX\\Noise.mp3",
+                "\\bin\\Composite\\SFX\\Organ.mp3",
+                "\\bin\\Composite\\SFX\\Short.mp3",
+                "\\bin\\Composite\\SFX\\Staccato.mp3"
+        };
+        String[] vocal = new String[] {
+                "\\bin\\Composite\\Vocals\\PayAttention.mp3",
+                "\\bin\\Composite\\Vocals\\Rrrah.mp3"
+        };
+
+        createLists(hat, kick, snare, bLoop, dLoop, pLoop, sfx, vocal);
+    }
+
+    private void createLists( String[] hat, String[] kick, String[] snare, String[] bLoop, String[] dLoop, String[] pLoop, String[] sfx, String[] vocal ) throws Exception {
+
+        for ( int i = 0; i < hat.length; i++ ) {
+
+            hatP.add(getClass().getResource(hat[ i ]).toURI().toString());
+            allP.add(getClass().getResource(hat[ i ]).toURI().toString());
+        }
+        for ( int i = 0; i < kick.length; i++ ) {
+
+            kickP.add(getClass().getResource(kick[ i ]).toURI().toString());
+            allP.add(getClass().getResource(kick[ i ]).toURI().toString());
+        }
+        for ( int i = 0; i < snare.length; i++ ) {
+
+            snareP.add(getClass().getResource(snare[ i ]).toURI().toString());
+            allP.add(getClass().getResource(snare[ i ]).toURI().toString());
+        }
+        for ( int i = 0; i < bLoop.length; i++ ) {
+
+            bLoopP.add(getClass().getResource(bLoop[ i ]).toURI().toString());
+            allP.add(getClass().getResource(bLoop[ i ]).toURI().toString());
+        }
+        for ( int i = 0; i < dLoop.length; i++ ) {
+
+            dLoopP.add(getClass().getResource(dLoop[ i ]).toURI().toString());
+            allP.add(getClass().getResource(dLoop[ i ]).toURI().toString());
+        }
+        for ( int i = 0; i < pLoop.length; i++ ) {
+
+            pLoopP.add(getClass().getResource(pLoop[ i ]).toURI().toString());
+            allP.add(getClass().getResource(pLoop[ i ]).toURI().toString());
+        }
+        for ( int i = 0; i < sfx.length; i++ ) {
+
+            sfxP.add(getClass().getResource(sfx[ i ]).toURI().toString());
+            allP.add(getClass().getResource(sfx[ i ]).toURI().toString());
+        }
+        for ( int i = 0; i < vocal.length; i++ ) {
+
+            vocalP.add(getClass().getResource(vocal[ i ]).toURI().toString());
+            allP.add(getClass().getResource(vocal[ i ]).toURI().toString());
+        }
+    }
 
 }
